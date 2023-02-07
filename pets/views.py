61c43ba5332f201baf94770c86db35f71d7237a7
Marija Simeonova <masimeonva@gmail.com>
2023-02-07 03:51:03 +0200

    account settings

--- pets/views.py
+++ pets/views.py
@@ -12,6 +12,26 @@ def index(request):
     return render(request, 'index.html')
 
 
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+        elif request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            
+        bio = request.POST['bio']
+        location = request.POST['location']
+
+        user_profile.profileimg = image
+        user_profile.bio = bio
+        user_profile.location = location
+        user_profile.save()
+        return redirect('settings')
+    return render(request, 'settings.html', {'user_profile': user_profile})
+
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -30,14 +50,14 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
                 # log user in and redirect to settigs page
-
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
                 # create a Profie object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(
                     user=user_model, id_user=user_model.id)
                 new_profile.save()
-                # change it to logged in page
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Passwords do not match')
             return redirect('signup')
