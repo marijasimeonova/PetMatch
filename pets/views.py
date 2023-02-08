73e187fc72135e91d33df8152a4984856a4a67d2
Marija Simeonova <masimeonva@gmail.com>
2023-02-08 19:20:46 +0200

    profile page

--- pets/views.py
+++ pets/views.py
@@ -53,6 +53,23 @@ def like_post(request):
         return redirect('/')
 
 
+# pk is the username of the currently viewed user
+@login_required(login_url='signin')
+def profile(request, pk):
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = Post.objects.filter(user=pk)
+    user_number_of_posts = len(user_posts)
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'user_number_of_posts': user_number_of_posts,
+    }
+
+    return render(request, 'profile.html', context)
+
+
 @login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
