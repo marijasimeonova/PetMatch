d66d3bee39c1e4bd5e95f297513df8888976155b
Marija Simeonova <masimeonva@gmail.com>
2023-02-10 16:49:43 +0200

    search for user (pet)

--- pets/views.py
+++ pets/views.py
@@ -4,15 +4,32 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, LikePost, FollowersCount
+from itertools import chain
 
 # Create your views here.
+
 
 @login_required(login_url='signin')
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
+
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(
+        follower=request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for user_name in user_following_list:
+        feed_lists = Post.objects.filter(user=user_name)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
+
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list})
 
 
 @login_required(login_url='signin')
@@ -30,13 +47,38 @@ def upload(request):
 
 
 @login_required(login_url='signin')
+def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in username_object:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+
+    username_profile_list = list(chain(*username_profile_list))
+
+    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
+
+
+@login_required(login_url='signin')
 def like_post(request):
     username = request.user.username
     post_id = request.GET.get('post_id')
 
     post = Post.objects.get(id=post_id)
-    #check if this post is already liked by the current user
-    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+    # check if this post is already liked by the current user
+    like_filter = LikePost.objects.filter(
+        post_id=post_id, username=username).first()
 
     if like_filter == None:
         # like post
@@ -62,7 +104,7 @@ def profile(request, pk):
     user_number_of_posts = len(user_posts)
     follower = request.user.username
     user = pk
-    
+
     if FollowersCount.objects.filter(follower=follower, user=user).first():
         button_text = 'Unfollow'
     else:
@@ -70,7 +112,7 @@ def profile(request, pk):
 
     user_number_of_followers = len(FollowersCount.objects.filter(user=pk))
     user_number_of_following = len(FollowersCount.objects.filter(follower=pk))
-    
+
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
@@ -91,16 +133,19 @@ def follow(request):
         user = request.POST['user']
         # the follower is already following the user -> unfollow the user
         if FollowersCount.objects.filter(follower=follower, user=user).first():
-            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower = FollowersCount.objects.get(
+                follower=follower, user=user)
             delete_follower.delete()
             return redirect('/profile/'+user)
         else:
-        # follow the user
-            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            # follow the user
+            new_follower = FollowersCount.objects.create(
+                follower=follower, user=user)
             new_follower.save()
             return redirect('/profile/'+user)
     else:
         return redirect('/')
+
 
 @login_required(login_url='signin')
 def settings(request):
@@ -110,7 +155,7 @@ def settings(request):
             image = user_profile.profileimg
         elif request.FILES.get('image') != None:
             image = request.FILES.get('image')
-            
+
         bio = request.POST['bio']
         location = request.POST['location']
 
@@ -137,10 +182,12 @@ def signup(request):
                 messages.info(request, 'Username already exists')
                 return redirect('signup')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password)
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
                 user.save()
                 # log user in and redirect to settigs page
-                user_login = auth.authenticate(username=username, password=password)
+                user_login = auth.authenticate(
+                    username=username, password=password)
                 auth.login(request, user_login)
                 # create a Profie object for the new user
                 user_model = User.objects.get(username=username)
@@ -169,7 +216,7 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request, 'signin.html')
-    
+
 
 @login_required(login_url='signin')
 def logout(request):
