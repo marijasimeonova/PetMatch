59591198352740effcc185317c29a8894fb57091
Marija Simeonova <masimeonva@gmail.com>
2023-02-17 19:54:26 +0200

    test models

--- pets/tests.py
+++ pets/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
-
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from .views import home, profile, upload, like_post, search, follow, settings, myposts, delete_post, signup, signin, logout
+from .models import Profile, Post, LikePost, Followers
+from django.contrib.auth.models import User
 
 # Create your tests here.
 
@@ -14,7 +15,7 @@ class TestUrls(SimpleTestCase):
         self.assertEquals(resolve(url).func, home)
 
     def test_profile_url_resolves(self):
-        url = reverse('profile', args = ['user'])
+        url = reverse('profile', args=['user'])
         self.assertEquals(resolve(url).func, profile)
 
     def test_upload_url_resolves(self):
@@ -56,3 +57,37 @@ class TestUrls(SimpleTestCase):
     def test_logout_url_resolves(self):
         url = reverse('logout')
         self.assertEquals(resolve(url).func, logout)
+
+
+class TestModels(TestCase):
+
+    def test_user_created(self):
+        user = User.objects.create_user(username='mia', email='mia@pets.com')
+        user.set_password('mia123')
+        user.save()
+        self.assertEqual(str(user), 'mia')
+
+    def test_profile_created(self):
+        user = User.objects.create_user(username='mia', email='mia@pets.com')
+        user.set_password('mia123')
+        user.save()
+        profile = Profile(user=user, id_user='123', bio='Hi, my name is mia', profileimg='',
+                          location='Sofia, Bulgaria', phone_number='+359 87 1233 125')
+        profile.save()
+        self.assertEqual(str(profile), 'mia')
+
+    def test_post_created(self):
+        post = Post(id='{12345678-1234-5678-1234-567812345678}', user='mia', image='', caption='hello',
+                    created_at='2023-02-17 07:04:03', number_of_likes='23')
+        post.save()
+        self.assertEqual(str(post), 'mia')
+
+    def test_post_is_liked(self):
+        likepost = LikePost(post_id='{12345678-1234-5678-1234-567812345678}', username='mia')
+        likepost.save()
+        self.assertEqual(str(likepost), 'mia')
+
+    def test_followers(self):
+        followers = Followers(follower='rex', user='mia')
+        followers.save()
+        self.assertEqual(str(followers), 'mia')
